dc6b6e8c5955855593e407d01af18d998ecf7660
YangZhendong <404013419@qq.com>
2017-03-29 14:39:19 -0600

    作业：隧道问题完成

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ###输入游民星空福利贴或者壁纸帖网址后自动下载所有子页面图片。
 ###v1.0：支持jpg格式抓取，超时会退出
 ###v1.1：新增支持超时重试，自动创建文件夹
-## 3.百度贴吧图片抓取工具tiebaPic+版本号.py
-## 4.知乎收藏夹图片抓取工具zhihushoucangPic.py
-## 5.知乎收藏夹问题抓取+Mysql保存zhihushoucangPic.py
+## 3.百度贴吧图片抓取工具: tiebaPic+版本号.py
+## 4.知乎收藏夹图片抓取工具: zhihushoucangPic.py
+## 5.知乎收藏夹问题抓取+Mysql保存: zhihushoucangPic.py
+## 6.隧道问题详见Assignment_1.pdf: tunnelsolve_final.py
--- tunnelsolve_final.py
+++ tunnelsolve_final.py
@@ -59,28 +59,56 @@ def get_distance_west():
         temp_height += 1
     return max_distance_west
 
-with open('test1.txt', 'r') as f:
-    number = 0
-    for line in f.readlines():
-        if (line != '\n') and number == 0:
-            up = line.split()
-            up = [int(x) for x in up if x]
-            # print(up)
-            number += 1
-        elif (line != '\n') and number == 1:
-            down = line.split()
-            down = [int(x) for x in down if x]
-            # print(down)
-            number += 1
-        elif (line != '\n') and number == 2:
-            sys.exit('wrong number of lines')
-        else:
-            continue
+def get_distance_east():
+    temp_height = down[-1] + 1
+    max_distance_east = 0
+    while temp_height <= up[-1]:
+        flag = 1
+        temp = -2
+        distance = 1
+        while flag:
+            try:
+                if temp_height <= down[temp] or temp_height > up[temp]:
+                    flag = 0
+                    continue
+                temp -= 1
+                distance += 1
+            except IndexError:
+                flag = 0
+                distance = 0
+        if max_distance_east < distance:
+            max_distance_east = distance
+        temp_height += 1
+    return max_distance_east
+
+file_name = input('Please enter the name of the file you want to get data from:')
+try:
+    with open(file_name, 'r') as f:
+        number = 0
+        for line in f.readlines():
+            if (line != '\n') and number == 0:
+                up = line.split()
+                up = [int(x) for x in up if x]
+                number += 1
+            elif (line != '\n') and number == 1:
+                down = line.split()
+                down = [int(x) for x in down if x]
+                number += 1
+            elif (line != '\n') and number == 2:
+                sys.exit('Sorry, input file does not store valid data.')
+            else:
+                continue
+except FileNotFoundError:
+    sys.exit('Sorry, input file does not store valid data.')
+except ValueError:
+    sys.exit('Sorry, input file does not store valid data.')
+if len(up) == 1 or len(down) == 1:
+    sys.exit('Sorry, input file does not store valid data.')
 if len(up) != len(down):
-    sys.exit('different numbers in lines')
+    sys.exit('Sorry, input file does not store valid data.')
 for x in range(len(up)):
     if down[x] >= up[x]:
-        sys.exit('wrong input')
+        sys.exit('Sorry, input file does not store valid data.')
 
 print('From the west, one can into the tunnel over a distance of %s' % get_distance_west())
 max_distance = 0
@@ -91,4 +119,7 @@ for x in range(len(down)-1):
     temp_distance = get_distance(1, x)
     if temp_distance > max_distance:
         max_distance = temp_distance
+max_distance_east = get_distance_east()
+if max_distance < max_distance_east:
+    max_distance = max_distance_east
 print('Inside the tunnel, one can into the tunnel over a maximum distance of %s' % max_distance)
