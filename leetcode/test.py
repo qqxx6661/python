6cb57bc4fe0970ea7dd2c35b95e351133283a434
YangZhendong <404013419@qq.com>
2017-08-18 11:44:22 -0600

    结构调整

--- leetcode/test.py
+++ leetcode/test.py
@@ -1,28 +1,19 @@
-# -*- coding: utf-8 -*-
-class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        result = []
-        index_i = 0
-        for i in range(len(nums)):
-            x = nums[i]
-            y = target - x
-            if y == x:  # 有输入[3,3]，所以需要找出两个3的位置，根据题目规定，不可能出现三个三
-                if nums.count(x) == 2:
-                    for num_same in nums:
-                        if x == num_same:
-                            result.append(index_i)
-                        index_i += 1
-                    return result
-            if y in nums:
-                result.append(i)
-                result.append(nums.index(y))
-                return result
+class Solution(object):
+    def FindTwoNodes(self, root):
+        if root:
+            self.FindTwoNodes(root.left)
+            if self.pre and self.pre.val >= root.val:
+                self.n2 = root
+                if not self.n1:
+                    self.n1 = self.pre
+            self.pre = root.val
+            self.FindTwoNodes(root.right)
 
-a = [1, 3, 3]
-s = Solution()
-print(s.twoSum(a, 6))
+    def recoverTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        self.n1, self.n2 = None, None
+        self.pre = None
+        self.FindTwoNodes(root)
